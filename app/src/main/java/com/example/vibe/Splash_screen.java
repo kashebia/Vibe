9c912a53ecfdd70fab20a0d071df86672f2d9ac4
kashebia <abhishekyadav123321@gmail.com>
2020-05-05 17:23:42 +0530

    commit2

--- app/src/main/java/com/example/vibe/Splash_screen.java
+++ app/src/main/java/com/example/vibe/Splash_screen.java
@@ -2,13 +2,24 @@ package com.example.vibe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_screen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(Splash_screen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
